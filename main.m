249908cb3887286a6792273e4f2dac1cc5194cfa
SKYnSPACE <skynspace@kaist.ac.kr>
2016-10-16 22:12:41 +0900

    Before changing line color
    Before changing line color

--- main.m
+++ main.m
@@ -39,3 +39,13 @@ title('Solutions of ideal nozzle');
 xlabel('Mach Number');
 set(get(ax(1), 'Ylabel'), 'String', 'Pressure ratio & Temperature ratio');
 set(get(ax(2), 'Ylabel'), 'String', 'Area ratio');
+
+%% Default MATLAB Plotting Color Sets from R2014b
+% 'Color', []
+%          0    0.4470    0.7410
+%     0.8500    0.3250    0.0980
+%     0.9290    0.6940    0.1250
+%     0.4940    0.1840    0.5560
+%     0.4660    0.6740    0.1880
+%     0.3010    0.7450    0.9330
+%     0.6350    0.0780    0.1840
